f5e3f2bfd565d860348d70119235d4dd8ff3563f
AAPythonCode <aapythonletscode@gmail.com>
2025-08-02 11:46:03 -0700

    Initial Commit

---
+++ NEWEssentialAI/NEWEssentialAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NEWEssentialAIApp.swift
+//  NEWEssentialAI
+//
+//  Created by Aarnav Dhir on 8/2/25.
+//
+
+import SwiftUI
+
+@main
+struct NEWEssentialAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NEWEssentialAITests/NEWEssentialAITests.swift
@@ -0,0 +1,17 @@
+//
+//  NEWEssentialAITests.swift
+//  NEWEssentialAITests
+//
+//  Created by Aarnav Dhir on 8/2/25.
+//
+
+import Testing
+@testable import NEWEssentialAI
+
+struct NEWEssentialAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
